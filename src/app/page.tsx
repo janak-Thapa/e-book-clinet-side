518c9f4560c8482e78aa99712033c9ddf9e48aa9
janak-Thapa <ogjanak1@gmail.com>
2024-05-24 13:02:08 +0545

    add banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import Banner from "@/components/Banner";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
   
-  <h1>Welcome to e-book client side</h1>
+      <Banner/>
 
   </>
   );
